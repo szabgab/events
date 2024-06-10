c31a08bec2ede8dbe474f71e2c592c74190caf5e
Gabor Szabo <gabor@szabgab.com>
2024-06-10 18:14:50 +0300

    filter out old events

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,6 @@
+use chrono::{DateTime, FixedOffset, Utc};
 use serde::{Deserialize, Serialize};
+use std::cmp::Ordering;
 use std::fs;
 
 #[derive(Deserialize, Serialize, Debug)]
@@ -18,10 +20,21 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
     let filename = "rust.yaml";
     let text = fs::read_to_string(filename).unwrap();
 
+    let now: DateTime<FixedOffset> = Utc::now().fixed_offset();
+    println!("now:  {now}");
+
     let events: Vec<Event> = serde_yaml::from_str(&text).unwrap_or_else(|err| {
         eprintln!("Could not parse YAML file: {err}");
         std::process::exit(1);
     });
+
+    let events = events
+        .iter()
+        .filter(|event| {
+            let dt = DateTime::parse_from_str(&event.start, "%Y-%m-%dT%H:%M:%S%z").unwrap();
+            dt.cmp(&now) != Ordering::Less
+        })
+        .collect::<Vec<&Event>>();
 
     let html = "";
     let template = include_str!("../templates/page.html");
