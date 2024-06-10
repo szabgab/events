868d13d5ea0cbf3b9a1821cb3b915e49b5a0df7f
Gabor Szabo <gabor@szabgab.com>
2024-06-10 14:58:34 +0300

    generate simple web page

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,20 @@
-fn main() {
-    println!("Hello, world!");
+fn main() -> Result<(), Box<dyn std::error::Error>> {
+    std::fs::create_dir_all("_site").unwrap();
+
+    let html = "Hello";
+    let template = include_str!("../templates/page.html");
+    let template = liquid::ParserBuilder::with_stdlib()
+        .build()
+        .unwrap()
+        .parse(template)
+        .unwrap();
+
+    let globals = liquid::object!({
+        "content": &html,
+        "title": "Rust Maven demo",
+    });
+    let output = template.render(&globals).unwrap();
+
+    std::fs::write("_site/index.html", output)?;
+    Ok(())
 }
